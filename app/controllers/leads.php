e4d19095ca2ca7b8e048741decdd32d05e3294dd
EvMaMis <evmamis13@gmail.com>
2024-04-02 19:42:05 +0300

    first stable version

--- app/controllers/leads.php
+++ app/controllers/leads.php
@@ -4,28 +4,23 @@ if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['addlead'])) {
     $api = curl_init();
 
     $data = [
-        'firstName' => $_POST['fisrtName'],
+        'firstName' => $_POST['firstName'],
         'lastName' => $_POST['lastName'],
         'phone' => $_POST['phone'],
         'email' => $_POST['email'],
         'box_id' => 28,
         'offer_id' => 5,
-        'countyCode' => 'GB',
+        'countryCode' => 'GB',
         'language' => 'en',
         'password' => 'qwerty12',
         'ip' => $_SERVER['REMOTE_ADDR'],
         'landingUrl' => $_SERVER['HTTP_REFERER'],
     ];
-    dd($data);
     curl_setopt($api, CURLOPT_URL, API_URL . 'addlead');
     curl_setopt($api, CURLOPT_POST, 1);
-    curl_setopt($api, CURLOPT_HTTPHEADER, [
-        'token' => TOKEN,
-        'Content-Type' => 'multipart/form-data',
-    ]);
-    curl_setopt($api, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($api, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'token: ' . TOKEN));
+    curl_setopt($api, CURLOPT_POSTFIELDS, json_encode($data));
     $output = curl_exec($api);
-    print_r('results');
     if(curl_errno($api)) {
         echo "Помилка CURL: " . curl_error($api);
     } else {
@@ -40,27 +35,18 @@ if($_SERVER['REQUEST_METHOD'] === 'GET') {
     $api = curl_init();
     curl_setopt($api, CURLOPT_URL, API_URL . 'getstatuses');
     curl_setopt($api, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($api, CURLOPT_HTTPHEADER, [
-        'token' => TOKEN,
-    ]);
+    curl_setopt($api, CURLOPT_HTTPHEADER, array("token:" . TOKEN));
+
     $leads = curl_exec($api);
     curl_close($api);
 
     if($leads === false) {
         echo "Ошибка получения данных";
+        dd(curl_error($api));
     } else {
         $statuses = json_decode($leads, true);
-        foreach ($statuses as $status) {
-            echo "<tr>";
-            echo "<td>" . $status['id'] . "</td>";
-            echo "<td>" . $status['email'] . "</td>";
-            echo "<td>" . $status['status'] . "</td>";
-            echo "<td>" . $status['ftd'] . "</td>";
-            echo "</tr>";
-        }
-
+        $leads = $statuses['data'];
     }
-    dd($leads);
 }
 
 function dd($data) {
